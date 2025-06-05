80e9d183978b0f0bb87ce4e4bbfffc0530efc2f4
NonPlayer95 <130722174+Nonplayer95@users.noreply.github.com>
2025-06-05 15:31:48 +0900

    revise my cord not much

--- HW02/HW02/Main.cpp
+++ HW02/HW02/Main.cpp
@@ -38,22 +38,23 @@ public:
 
 int main()
 {
-	Animal* animals[3];// 포인터 배열선언이라 크기 초기화를 해야하는데 크기를 뭘로 잡아야하는지 모르겟음
+	Animal* animals[3];// 일단 dog cat cow 3종류의 동물이니까 담음
 	
-	dog;// 실제 사용 가능한 놈으로 만들어야한다, class는 설계도고, 실제 객체로 소환되는 놈은 다르니까
-	realcat=cat; // ??
-	cow;
+	dog; // 실제 사용 가능한 놈으로 만들어야한다, class는 설계도고, 실제 객체로 소환되는 놈은 다르니까
+	cat; //realcat=cat; << 사용안됨
+	cow; // 변수선언을 다시해야하는데
+
+	// int a; 이런식으로 말이다.
 
 
-	int a;
-	int dog;// dog란 변수를 새로 만드는거니깐 안됨 이렇게 쓰면
+
 
 	// 배열을 반복문으로 순회하면서 울음소리 내기
 
 	cout << "각 동물들의 울음소리 " << endl;
-	for (int i = 0; i < ? ; i++)
+	for (int i = 0; i < 3 ; i++) // 크기가 3인 배열이니까
 	{
-		// 여기서 i릉 어떻게 써먹어야하는가
+		// animals[i] > makesound(); 이런식으로 짜야하나?
 	}
 	
 
